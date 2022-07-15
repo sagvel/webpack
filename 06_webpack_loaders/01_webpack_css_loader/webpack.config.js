5263edba406a1ce0bc621f15f9d63f5a05e763b4
sagvel <saulko.oleksander@gmail.com>
2022-07-15 17:25:55 +0300

    add 6.1 solution

---
+++ 06_webpack_loaders/01_webpack_css_loader/webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /.c?ss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /.(jpg|png)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              name: '[name].[ext]',
+              outputPath: 'images',
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
